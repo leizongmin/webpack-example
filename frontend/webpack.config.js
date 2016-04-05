2f3d7d5f13523bd8b22ab7599f6d85128ba1a158
Zongmin Lei <leizongmin@gmail.com>
2016-04-05 16:38:06 +0800

    bootstrap

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
       { test: /\.(woff|woff2)$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
       { test: /\.ttf$/, loader: 'file-loader' },
       { test: /\.eot$/, loader: 'file-loader' },
-      { test: /\.svg$/, loader: 'file-loader' }
+      { test: /\.svg$/, loader: 'file-loader' },
+      {
+        test: /\/bootstrap\/js\//,
+        loader: 'imports?jQuery=jquery'
+      },
     ]
   }
 };
