905c99aff01977b5edc88c05c9949b59e595ba0a
Zongmin Lei <leizongmin@gmail.com>
2016-04-05 18:28:04 +0800

    React

--- frontend/component/List.js
+++ frontend/component/List.js
@@ -1,1 +1,29 @@
+'use strict';
 
+import React from 'react';
+import apiClient from '../lib/api_client';
+
+export default class List extends React.Component {
+
+  state = {
+    list: [],
+  };
+
+  componentDidMount() {
+    apiClient.getList({})
+    .then(list => this.setState({list}))
+    .catch(err => console.error(err));
+  }
+
+  render() {
+    const list = this.state.list.map(item => {
+      return <a href="#" className="list-group-item" key={item.id}>{item.title}</a>;
+    });
+    return (
+      <ul class="list-group">
+        {list}
+      </ul>
+    );
+  }
+
+}
--- frontend/test.js
+++ frontend/test.js
@@ -1,11 +1,17 @@
+'use strict';
+
 import 'bootstrap-webpack';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import apiClient from './lib/api_client';
+import List from './component/List';
 
 class App extends React.Component {
   render() {
-    return <h1>hello</h1>;
+    return (
+      <div>
+        <List />
+      </div>
+    );
   }
 }
 
@@ -13,7 +19,3 @@ const app = document.createElement('div');
 app.id = 'app';
 document.body.appendChild(app);
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-const print = console.log.bind(console);
-apiClient.getList({}).then(print).catch(print);
